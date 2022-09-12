80b843578140fa224820b26142463583ebd126de
Guy Siedes <3strategy@gmail.com>
2022-09-12 13:53:18 +0200

    matched the vs2022 turtle 22 from today

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Turtle19
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to run \nmain 1 (Turtle) or \n2 (Bucket)?\n3 (Frog)?" +
+                "\n\n\n\t\ttype 1, 2, or 3");
+            int n = int.Parse(Console.ReadKey().KeyChar.ToString());
+            if (n == 1)
+            {
+                Main1();
+            }
+            else if (n == 2)
+                Main2();
+            else if (n == 3)
+                Main3();
+            else
+                Console.WriteLine("You selected an invalid option");
+        }
+    }
+}
---
+++ Program3Frog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4.FrogLib;
+
+namespace Turtle19
+{
+    internal partial class Program
+    {
+        static void Main3()
+        {
+            BrownFrog f0 = new BrownFrog(4, "f0");
+            BrownFrog f1 = new BrownFrog(5, "f1");
+            BrownFrog f2 = new BrownFrog(6, "f2");
+
+            GreenFrog f4 = new GreenFrog(0, "f4"); 
+            GreenFrog f5 = new GreenFrog(1, "f5"); 
+            GreenFrog f6 = new GreenFrog(2, "f6");
+
+
+        }
+    }
+}
